fb3956ea301d4ac8593e76f44158068c1580b29c
peatahaya <pantherbielicki2@vp.pl>
2024-02-14 23:37:26 +0100

    Creating simple weather app

---
+++ playground.js
@@ -0,0 +1,11 @@
+// Baby weather app
+// if rain -> 'Grab your umbrella'
+// else -> 'Wear your sunglasses'
+
+let weather = 'rain'
+
+if (weather == 'rain') {
+    console.log('Grab your umbrella');
+} else {
+    console.log('Wear your sunglasses');
+}
