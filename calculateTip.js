7dee3303a2c8a1058d466ac017e3bb847102fa76
peatahaya <pantherbielicki2@vp.pl>
2024-02-19 06:23:33 +0100

    updating calculateTips.js, adding lists.js and objects.js

--- calculateTip.js
+++ calculateTip.js
@@ -2,8 +2,10 @@ function sum(a, b) {
 	return a + b;
 }
 
+// arrow function with implicit return
 const sumArrow = (a, b) => a + b;
 
+// 
 function calculateFoodTotal(food, tip) {
 	tipPercentage = tip / 100;
 	tipAmount = food * tipPercentage;
---
+++ lists.js
@@ -0,0 +1,5 @@
+let fruits = ['apple', 'banana', 'pear'];
+console.log(fruits);
+fruits.push('cherry');
+console.log(fruits);
+console.log(fruits.slice(0,3))
